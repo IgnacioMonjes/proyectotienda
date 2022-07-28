51dd09da67de9d3b3b15ba7264da6f0745b32d56
Ignacio Monjes <Ignacio.Monjes@assistcard.com>
2022-07-27 21:01:14 -0300

    Ejercicios Clase 3 sin funcionar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  
+  const handleOnAdd =(quantity) =>{
+    console.log(`La cantidad agregada es: ${quantity}`)
+  }
+  
   return (
       <div className="App">
-        <ItemListContainer greeting='Arcilla con Prop'/>
         <Navbar />
+        <ItemListContainer greeting='Arcilla con Prop'/>
+        <Counter stock={10}  onAdd={handleOnAdd}/>
       </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,27 @@
+import './ItemListContainer.css'
+import { useState, useEffect } from 'react'
+import { getProducts } from '../../asyncMock'
+import ItemList from '../ItemList/ItemList'
 
 const ItemListContainer = ({ greeting }) =>{
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProducts().then(products => {
+            setProducts(products)
+        })   
+    },[])
+
     return (
-        <h1>{greeting}</h1>
+        <>
+            <h1>{greeting}</h1>
+            
+            {/*<ul > */}
+                {/*products.map(prod => <li key= {prod.id}>{prod.name}</li>)*/}
+            {/*</ul> */}
+*/           <ItemList products ={products}/> 
+        </>
     )
 }
 
